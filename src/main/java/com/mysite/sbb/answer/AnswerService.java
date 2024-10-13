7c4c7d523421492cf2140408487a4fd533cce3da
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-13 22:40:56 +0900

    Create AnswerService.java

---
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,9 @@
+package com.mysite.sbb.answer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+}
