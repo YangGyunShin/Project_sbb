af6b81afc3348b163cc3ac9e9869f50a7f2e7f6d
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-13 20:58:37 +0900

    Update QuestionController.java

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,15 +1,26 @@
 package com.mysite.sbb.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
+    // RequiredArgsConstructor을 통해 객체를 자동주입
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    @ResponseBody
-    public String list() {
-        return "question list";
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        // model 객체는 자바클래스와 템플릿 간의 연결고리를 역할함.
+        // model 객체는 따로 생성할 필요 없이 컨트롤러의 메서드에 매개변수로 지정하기만 하면 됨.
+        model.addAttribute("questionList", questionList);
+        return "question_list";
     }
 }
