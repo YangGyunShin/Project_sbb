281db2fdf3fe7f4ea80b2bcfe91b5d174f83087a
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-13 14:59:47 +0900

    Update SbbApplicationTests.java

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,65 @@
 package com.mysite.sbb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		/*Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);*/
+
+		/*List<Question> all = this.questionRepository.findAll();
+		assertEquals(10, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if (oq.isPresent()) {
+			// oq 값이 존재한다면, get()을 통해 실제 Question 객체의 값을 얻는다.
+			Question qn = oq.get();
+			assertEquals("sbb가 무엇인가요?", qn.getSubject());
+		}*/
+
+		/*Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q.getId());*/
+
+		/*Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());*/
+
+		/*List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());*/
+
+		Optional<Question> oq = this.questionRepository.findById(1);
+		// assertTrue : 괄호 안의 값이 true인지를 테스트함. 만약 false라면, 오류가 발생하고 테스트는 종료됨
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
 	}
 
 }
