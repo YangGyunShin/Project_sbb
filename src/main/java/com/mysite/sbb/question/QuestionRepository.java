65ab31e1494982746fd9f9eaa239d0e600c3b197
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-21 22:18:22 +0900

    Update QuestionRepository.java

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -3,7 +3,7 @@ package com.mysite.sbb.question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 // <Qustion, Integer> : Question 엔터티로 리포지터리를 생성하며, Question 엔터티의 기본키는 Integer임
