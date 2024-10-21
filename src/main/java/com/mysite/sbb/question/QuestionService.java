29491cc45a2f784b59eb61978a5be059ed2b64ba
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-21 22:18:20 +0900

    Update QuestionService.java

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -3,10 +3,12 @@ package com.mysite.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +17,13 @@ import java.util.Optional;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
+    }
 
     public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
@@ -31,11 +40,5 @@ public class QuestionService {
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
-    }
-
-    public Page<Question> getList(int page) {
-        Pageable pageable = (Pageable) PageRequest.of(page, 10);
-        return this.questionRepository.findAll(pageable);
-
     }
 }
