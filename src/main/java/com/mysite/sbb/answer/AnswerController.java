d9e7aa9b28c0ad719ea48dc7aed3d495dd7f5a5c
YangGyunShin <skshtlsdidrb@gmail.com>
2024-10-24 23:10:44 +0900

    Update AnswerController.java

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,8 +2,11 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,17 +24,20 @@ public class AnswerController {
 
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final UserService userService;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id,
                                @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
         Question question = this.questionService.getQuestion(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
         // 검증에 실패할 경우, 다시 답변을 등록할 수 있는 question_detail 템플릿을 출력하게 된다.
         // 이때 question_detail 템플릿은 Question 객체가 필요하므로, model 객체에 question 객체를 저장한 후 question_detail 템플릿을 출력해야 함.
         if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
             return "question_detail";
-        } this.answerService.create(question, answerForm.getContent());
+        } this.answerService.create(question, answerForm.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s", id);
     }
 }
